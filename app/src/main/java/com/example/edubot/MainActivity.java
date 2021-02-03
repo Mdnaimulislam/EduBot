933146f7d790f04b2e599182b103c2944ebb1ff1
Mdnaimulislam <51947096+Mdnaimulislam@users.noreply.github.com>
2021-02-03 17:56:11 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/edubot/MainActivity.java
+++ app/src/main/java/com/example/edubot/MainActivity.java
@@ -547,7 +547,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         //Wait for user response
         //System.out.println("\n\nType something that you want me to search on the internet...");
         //String nextLine = scanner.nextLine();
-        String searchText = wikiSearch +" wikiPedia";
+        String searchText = wikiSearch +" simple wiki english";
         //System.out.println("Searching on the web....");
 
         Document google = Jsoup.connect("https://www.google.com/search?q="+searchText).get();
